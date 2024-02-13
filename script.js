1058bbf6d1a6f6be5e39865c5e6ca2c469d05e43
praveshsuthar9 <praveshsuthar9@gmail.com>
2024-02-13 21:58:15 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("password");
+const passwordBox = document.getElementById("generate-p");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+<>{}[]/-=";
+
+const allChares = upperCase + lowerCase + number + symbol;
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChares[Math.floor(Math.random() * allChares.length)];
+  }
+
+  passwordBox.value = password;
+}
